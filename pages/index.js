3810aaaa8e9e17b8a96c59e9f80268b4ce56b9c1
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-09 17:48:36 +0900

    styles jsx

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,15 @@ import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
+    // styles jsx 해당 컴포넌트 내에서만 작동하므로 아래 코드들은 적용되지 않음
     <div>
       <NavBar />
-      <h1>Hello</h1>
+      <h1 className="active">Hello</h1>
+      <style jsx>{`
+        a {
+          color: white;
+        }
+      `}</style>
     </div>
   );
 }
