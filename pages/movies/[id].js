ac5636f7aea3d8516c5ec68568a1df86df69855c
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-03-10 23:07:42 +0900

    mask url and route hook

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -2,6 +2,11 @@ import { useRouter } from "next/router";
 
 export default function Detail() {
   const router = useRouter();
-  console.log(router);
-  return "detail";
+  // console.log(router);  // url이 전하는 데이터를 콘솔로 볼 수 있다.
+
+  return (
+    <div>
+      <h4>{router.query.title || "Loading..."}</h4>
+    </div>
+  );
 }
